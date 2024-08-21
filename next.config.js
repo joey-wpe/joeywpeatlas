08a71d8333a6117124d4bd54475b8430f48cc829
Joey Teng <joey.teng@wpengine.com>
2024-08-20 22:01:51 -0600

    Next Config and Install Atlas Next

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const { withFaust, getWpHostname } = require('@faustwp/core');
+const { withFaust } = require('@faustwp/core');
+const { withAtlasConfig } = require('@wpengine/atlas-next');
+const { getWpHostname } = require('@faustwp/core');
 
-/**
- * @type {import('next').NextConfig}
- **/
-module.exports = withFaust({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: ['node_modules'],
@@ -15,4 +15,7 @@ module.exports = withFaust({
     locales: ['en'],
     defaultLocale: 'en',
   },
-});
+};
+
+// Combine the configurations using both `withFaust` and `withAtlasConfig`
+module.exports = withFaust(withAtlasConfig(nextConfig));
