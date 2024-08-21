cc7ffc65a8f148fa3499750387c5e70416a2ebb2
Joey Teng <joey.teng@wpengine.com>
2024-08-21 07:55:10 -0600

    Test ISR

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,21 +1,19 @@
 export default async function handler(req, res) {
-    // Check for a secret token to authenticate the request
-    if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-  
-    const path = req.query.path;
-  
-    // Ensure the path parameter is provided
-    if (!path) {
-      return res.status(400).json({ message: 'Path query parameter is required' });
-    }
-  
-    try {
-      // Revalidate the specified path
-      await res.revalidate(path);
-      return res.json({ revalidated: true });
-    } catch (err) {
-      return res.status(500).json({ message: 'Error revalidating', error: err.message });
-    }
+  console.log('Token:', process.env.MY_SECRET_TOKEN); // Log the token to check
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
   }
+
+  const path = req.query.path;
+
+  if (!path) {
+    return res.status(400).json({ message: 'Path query parameter is required' });
+  }
+
+  try {
+    await res.revalidate(path);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).json({ message: 'Error revalidating', error: err.message });
+  }
+}
